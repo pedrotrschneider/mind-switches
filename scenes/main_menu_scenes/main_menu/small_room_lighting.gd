39156d373cbea1504e1da25430b1f16cbeaca8dc
pedrotrschneider <pedrotrschneider@gmail.com>
2021-03-05 19:04:16 -0300

    ⚡️ Improve lighting on the main menu
    Add world environment for better lighting.

--- scenes/main_menu_scenes/main_menu/small_room_lighting.gd
+++ scenes/main_menu_scenes/main_menu/small_room_lighting.gd
@@ -3,4 +3,4 @@ extends Spatial
 export(NodePath) onready var _animation_player = get_node(_animation_player) as AnimationPlayer;
 
 func _ready():
-	_animation_player.play("OscilatingLight", -1, 0.3);
+	_animation_player.play("OscilatingLight", -1, 0.2);
