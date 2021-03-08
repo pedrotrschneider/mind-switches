4992f809aefff1fc939674284c78d21e321496f5
pedrotrschneider <pedrotrschneider@gmail.com>
2021-03-08 11:47:18 -0300

    ✨ Improve the main menu screen and trasition - Improve lighting - Add table and brain to the scene - Add transition form one scene to another

--- scenes/app/app.gd
+++ scenes/app/app.gd
@@ -2,6 +2,9 @@ extends Spatial
 
 var _garbage;
 
+export(NodePath) onready var _animation_player = get_node(_animation_player) as AnimationPlayer;
+export(NodePath) onready var _fade_rect = get_node(_fade_rect) as ColorRect;
+
 onready var _main_menu_res: Resource = preload("res://scenes/main_menu_scenes/main_menu/main_menu.tscn");
 onready var _level_res: Resource = preload("res://scenes/game_scenes/level/level.tscn");
 
@@ -10,10 +13,23 @@ func _ready():
 	self.add_child(main_menu_instance);
 	
 	_garbage = GameEvents.connect("go_to_level", self, "_on_go_to_level");
+	_garbage = GameEvents.connect("door_selected", self, "_on_door_selected");
+
+
+func _on_door_selected() -> void:
+	_fade_rect.show();
+	_animation_player.play("Fade", -1, (1 / 0.6));
+	yield(_animation_player, "animation_finished");
+	_fade_rect.hide();
 
 
 func _on_go_to_level() -> void:
-	self.get_child(1).queue_free();
+	self.get_child(get_child_count() - 1).queue_free();
 	
 	var level_instance = _level_res.instance();
 	self.add_child(level_instance);
+	
+	_fade_rect.show();
+	_animation_player.play_backwards("Fade");
+	yield(_animation_player, "animation_finished");
+	_fade_rect.hide();
